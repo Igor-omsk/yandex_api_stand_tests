c22dec45d2903c9eae9ce55c3de316c2de98c569
Igor_omsk <igor_omsk@mail.ru>
2024-10-28 20:24:05 +0600

    My first commit

---
+++ configuration.py
@@ -0,0 +1,20 @@
+# URL_SERVICE хранит базовый URL веб-сервиса, который используется для доступа к API или другим ресурсам.
+# Значение должно быть скопировано из настроек или документации сервиса, к которому предоставляется доступ.
+# Пример значения: "https://api.example.com"
+URL_SERVICE = "https://07adb2e4-2e0c-4526-b061-e676ffcbb370.serverhub.praktikum-services.ru"
+
+# DOC_PATH содержит путь к документации веб-сервиса.
+# Этот путь используется для формирования полного URL пути к документации, добавляя его к базовому URL сервиса.
+# В результате получится что-то вроде "https://api.example.com/docs/"
+DOC_PATH = "/docs/"
+
+# LOG_MAIN_PATH содержит путь до логов сервера
+LOG_MAIN_PATH = "/api/logs/main/"
+
+# USERS_TABLE_PATH содержит путь до таблицы
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+
+#CREATE_USER_PATH хранит путь к API-методу для создания нового пользователя
+CREATE_USER_PATH = "/api/v1/users/"
+
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
---
+++ sender_stand_request.py
@@ -0,0 +1,53 @@
+# Импортируем модуль configuration, который, мы создали выше - он содержит настройки подключения и путь к документации
+from email.feedparser import headerRE
+from http.client import responses
+
+import configuration
+
+# Импортируем модуль requests, который предназначен для отправки HTTP-запросов
+# Это популярная библиотека, которая позволяет взаимодействовать с веб-сервисами
+import requests
+
+import data
+
+
+#Этот код отправляет HTTP GET-запрос к заданному URL-адресу, который складывается
+#из базового адреса сервиса и пути к его документации, оба определены в модуле
+#конфигурации. Затем он выводит HTTP-статус код ответа от сервера, который указывает
+#на результат выполнения запроса.
+
+# Определяем функцию get_docs, которая не принимает параметров
+def get_docs():
+    # Выполняем GET-запрос к URL, который складывается из базового URL-адреса сервиса
+    # и пути к документации, заданных в модуле конфигурации
+    # Функция возвращает объект ответа от сервера
+    return requests.get(configuration.URL_SERVICE + configuration.DOC_PATH)
+
+# Определяем функцию get_logs, которая не принимает параметров
+def get_logs():
+    return requests.get(configuration.URL_SERVICE + configuration.LOG_MAIN_PATH, params={'count':20})
+
+# Определяем функцию get_users_table, которая не принимает параметров
+def get_users_table():
+    return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH)
+
+
+
+# Определение функции post_new_user для отправки POST-запроса на создание нового пользователя
+def post_new_user(body):
+    # Выполнение POST-запроса с использованием URL из конфигурационного файла, тела запроса и заголовков
+    # URL_SERVICE и CREATE_USER_PATH объединяются для формирования полного URL для запроса
+    # json=body используется для отправки данных пользователя в формате JSON
+    # headers=data.headers устанавливает заголовки запроса из модуля data
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
+                         json=body,
+                         headers=data.headers)
+
+def post_products_kits(products_ids):
+    # Отправка POST-запроса с использованием URL из конфигурации, данных о продуктах и заголовков
+    # Возвращается объект ответа, полученный от сервера
+    return requests.post(configuration.URL_SERVICE + configuration.PRODUCTS_KITS_PATH,
+                         json=products_ids,  # Тело запроса содержит ID продуктов в формате JSON
+                         headers=data.headers)  # Использование заголовков из файла data.py
+
+
